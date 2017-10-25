1b9811d86a1a86bdfcd45a845d999816a99be1f3
Masahiko Sato <sato_m@mediaxis.jp>
2017-10-25 18:54:35 +0900

    add Stack

---
+++ Stack/Stack.playground/Contents.swift
@@ -0,0 +1,39 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+public struct Stack<T> {
+    fileprivate var array = [T]()
+    
+    var count: Int {
+        return array.count
+    }
+    
+    var isEmpty: Bool {
+        return array.isEmpty
+    }
+    
+    mutating func push(_ element: T) {
+        array.append(element)
+    }
+    
+    mutating func pop() -> T? {
+        return array.popLast()
+    }
+    
+    var top: T? {
+        return array.last
+    }
+}
+
+var stackOfName = Stack(array: ["Pippo", "Kaka", "Nesta", "Maldini"])
+stackOfName.count
+
+stackOfName.push("Pirlo")
+print(stackOfName.array)
+
+stackOfName.pop()
+
+stackOfName.top
